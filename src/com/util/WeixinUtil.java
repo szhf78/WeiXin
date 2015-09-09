11a641769618814bb12d6ccc2ab4e54925498ebe
szhf78 <szhf78@163.com>
2015-09-10 00:06:29 +0800

    Revert "用户信息"
    This reverts commit d043be2e0a1322e6cab44403bc2f2996897e19f2.

--- src/com/util/WeixinUtil.java
+++ src/com/util/WeixinUtil.java
@@ -19,159 +19,113 @@ import org.slf4j.LoggerFactory;
 
 import com.vo.Token;
 
+
 /**
  * 通用工具类
- * 
  * @author Franco.Han
  * @date 2015-9-8
  * @version
  */
 public class WeixinUtil {
-	private static Logger log = LoggerFactory.getLogger(WeixinUtil.class);
-
-	// 凭证获取(get)
-	public static String token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
-
+	private static Logger log=LoggerFactory.getLogger(WeixinUtil.class);
+	
+	//凭证获取(get)
+	public static String token_url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+	
 	/**
 	 * 发送https请求
-	 * 
-	 * @param requestUrl
-	 *            请求地址
-	 * @param requestMethod
-	 *            请求方式(GET,POST)
-	 * @param outputStr
-	 *            提交数据
+	 * @param requestUrl 请求地址
+	 * @param requestMethod 请求方式(GET,POST)
+	 * @param outputStr 提交数据
 	 * @return JSONObject(通过JSONObject.get(key)的方式获取json对象的属性)
 	 */
-	public static JSONObject httpsRequest(String requestUrl,
-			String requestMethod, String outputStr) {
-		JSONObject jsonObject = null;
+	public static JSONObject httpsRequest(String requestUrl,String requestMethod,String outputStr){
+		JSONObject jsonObject=null;
 		try {
-			// 创建SSLContext对象,并使用我们指定的信任管理器初始化
-			TrustManager[] tm = { new MyX509TrustManager() };
-			SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+			//创建SSLContext对象,并使用我们指定的信任管理器初始化
+			TrustManager[] tm={new MyX509TrustManager()};
+			SSLContext sslContext=SSLContext.getInstance("SSL", "SunJSSE");
 			sslContext.init(null, tm, new java.security.SecureRandom());
-
-			// 从上述SSLContext对象中得到SSLSocketFactory对象
-			SSLSocketFactory ssf = sslContext.getSocketFactory();
-
-			URL url = new URL(requestUrl);
-
-			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-
+			
+			//从上述SSLContext对象中得到SSLSocketFactory对象
+			SSLSocketFactory ssf=sslContext.getSocketFactory();
+			
+			URL url=new URL(requestUrl);
+			
+			HttpsURLConnection conn=(HttpsURLConnection) url.openConnection();
+			
 			conn.setSSLSocketFactory(ssf);
-
+			
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
 			conn.setUseCaches(false);
-			// 设置请求方式(GET/POST)
+			//设置请求方式(GET/POST)
 			conn.setRequestMethod(requestMethod);
-
-			// 当outputStr不为null时向输出流写数据
-			if (null != outputStr) {
-				OutputStream outputStream = conn.getOutputStream();
-				// 编码格式
+			
+			//当outputStr不为null时向输出流写数据
+			if(null!=outputStr){
+				OutputStream outputStream=conn.getOutputStream();
+				//编码格式
 				outputStream.write(outputStr.getBytes("UTF-8"));
 				outputStream.close();
 			}
-
-			// 从输入流读取返回内容
-			InputStream inputStream = conn.getInputStream();
-
-			InputStreamReader inputStreamReader = new InputStreamReader(
-					inputStream);
-
-			BufferedReader bufferedReader = new BufferedReader(
-					inputStreamReader);
-
-			String str = null;
-			StringBuffer buffer = new StringBuffer();
-
-			while ((str = bufferedReader.readLine()) != null) {
+			
+			//从输入流读取返回内容
+			InputStream inputStream=conn.getInputStream();
+			
+			InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+			
+			BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+			
+			String str=null;
+			StringBuffer buffer=new StringBuffer();
+			
+			while((str=bufferedReader.readLine())!=null){
 				buffer.append(str);
 			}
-
-			// 释放资源
+			
+			//释放资源
 			bufferedReader.close();
 			inputStreamReader.close();
 			inputStream.close();
-			inputStream = null;
+			inputStream=null;
 			conn.disconnect();
-			jsonObject = JSONObject.fromObject(buffer.toString());
-
-		} catch (ConnectException ce) {
-			log.error("连接超时:{}", ce);
-		} catch (Exception e) {
-			log.error("https请求异常:{}", e);
+			jsonObject=JSONObject.fromObject(buffer.toString());
+			
+		}catch (ConnectException ce) {
+			log.error("连接超时:{}",ce);
+		}catch (Exception e) {
+			log.error("https请求异常:{}",e);
 		}
 		return jsonObject;
 	}
-
+	
 	/**
 	 * 获取接口访问凭证
-	 * 
 	 * @param appid
 	 * @param appsecret
 	 * @return
 	 */
-	public static Token getToken(String appId, String appSecret) {
-		Token token = null;
-		String requestUrl = token_url.replace("APPID", appId).replace(
-				"APPSECRET", appSecret);
-
-		// 发起GET请求获取凭证
-		JSONObject jsonObject = httpsRequest(requestUrl, "GET", null);
-
-		if (null != jsonObject) {
+	public static Token getToken(String appId,String appSecret){
+		Token token=null;
+		String requestUrl=token_url.replace("APPID", appId).replace("APPSECRET", appSecret);
+		
+		//发起GET请求获取凭证
+		JSONObject jsonObject=httpsRequest(requestUrl, "GET", null);
+		
+		if(null!=jsonObject){
 			try {
-				token = new Token();
-
+				token=new Token();
+				
 				token.setAccessToken(jsonObject.getString("access_token"));
 				token.setExpiresIn(jsonObject.getInt("expires_in"));
 			} catch (Exception e) {
-				token = null;
-				// 获取token失败
-				log.error("获取token失败 errcode:{} errmsg:{}", jsonObject);
+				token=null;
+				//获取token失败
+				log.error("获取token失败 errcode:{} errmsg:{}",jsonObject);
 			}
-
+			
 		}
 		return token;
-	}
-
-	/**
-	 * URL编码(utf-8)
-	 * 
-	 * @param source
-	 * @return
-	 */
-	public static String urlEncodeUTF8(String source) {
-		String result = source;
-		try {
-			result = java.net.URLEncoder.encode(source, "utf-8");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	
-	/**
-	 * 根据内容类型判断文件扩展名
-	 * @param contentType　内容类型
-	 * @return
-	 */
-	public static String getFileExt(String contentType){
-		String fileExt="";
-		if("image/jpeg".equals(contentType))
-			fileExt=".jpg";
-		else if("audio/mpeg".equals(contentType))
-			fileExt=".mp3";
-		else if("audio/amr".equals(contentType))
-			fileExt=".amr";
-		else if("video/mp4".equals(contentType))
-			fileExt=".mp4";
-		else if("video/mpeg4".equals(contentType))
-			fileExt=".mp4";
-		return fileExt;
 	}
 }
